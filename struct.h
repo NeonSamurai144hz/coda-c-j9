03efe0acd0c772105f4857e86e9508879a5c57ef
Bobeica Nikita <nbobeica@DESKTOP-44P6421>
2024-10-08 14:39:08 +0200

    EX FINI

---
+++ struct.h
@@ -0,0 +1,18 @@
+
+#ifndef TESTS_STRUCT_H  
+#define TESTS_STRUCT_H  
+  
+
+  struct s_char 
+  {
+    int strenght;
+    int dexterity;
+    int intelligence;
+    int agility;
+    int endurance;
+    int wisdom;
+    int faith;
+    int vigor;
+  };
+  typedef struct s_char character;
+  #endif
